958d86b0349fbd6051bf768d6e7fa662d4364bd5
Himanshu Singh <himanshusingh8859@yahoo.in>
2022-12-19 02:15:10 +0530

    better shortcut name

--- src/utils/shortcut.js
+++ src/utils/shortcut.js
@@ -1,9 +1,9 @@
 const queryCheatSheet = {
-    '/seltable': 'select * from <TABLE_NAME>',
-    '/selcol': 'select <COLUMN1,COLUMN2> from <TABLE_NAME>',
-    '/selfilter': 'select <COLUMN1,COLUMN2> from <TABLE_NAME> where <CONDITION>',
-    '/selcount': 'select COUNT(*) from <TABLE_NAME>',
-    '/seltext': 'select <COLUMN1> from <TABLE_NAME> where <COLUMN1> LIKE `P%` OR <COLUMN1> LIKE `%s`'
+    '/table': 'select * from <TABLE_NAME>',
+    '/column': 'select <COLUMN1,COLUMN2> from <TABLE_NAME>',
+    '/filter': 'select <COLUMN1,COLUMN2> from <TABLE_NAME> where <CONDITION>',
+    '/count': 'select COUNT(*) from <TABLE_NAME>',
+    '/text': 'select <COLUMN1> from <TABLE_NAME> where <COLUMN1> LIKE `P%` OR <COLUMN1> LIKE `%s`'
 }
 
 export const getSQLQueryCheat = (shortcut) => {
